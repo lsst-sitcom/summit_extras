2376f4897c273003e7565f38c728d8a4a480dc4a
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2022-07-07 13:18:02 -0500

    Add some extra features

--- python/lsst/summit/extras/logUtils.py
+++ python/lsst/summit/extras/logUtils.py
@@ -40,16 +40,22 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
+import math
 import lsst.summit.utils.butlerUtils as butlerUtils
 
 __all__ = ["LogBrowser",
            ]
 
-# _LOG = logging.getLogger(__name__)
-_LOG = logging.getLogger('lsst.XXX.removeThis')
+_LOG = logging.getLogger(__name__)
 
 
 class LogBrowser():
+    # add the end of the common part of any similar errors you want collapsed here
+    # this part of the string won't be lost, but is used to classify things
+    # as the same animal
+    SPECIAL_ZOO_CASES = ['with gufunc signature (n?,k),(k,m?)->(n?,m?)',
+                         ]
+
     def __init__(self, taskName, collection, doCache=True):
         self.taskName = taskName
         self.collection = collection
@@ -105,9 +111,18 @@ class LogBrowser():
         return logs
 
     def getPasses(self):
-        raise NotImplementedError
-        # for record, log in self.logs.items():
-        #     if log[-1].message.find('failed') != -1:
+        fails = self.getFailRecords()
+        passes = [r for r in self.expRecords if r not in fails]
+        return passes
+
+    def printPasses(self):
+        passes = self.getPasses()
+        for record in passes:
+            print(self.recordToDataId(record))
+        # return sortRecordsByDayObsThenSeqNum(passes)
+
+    def countFails(self):
+        print(f"{len(self.getFailRecords())} fail cases found")
 
     def getFailRecords(self):
         fails = []
@@ -126,4 +141,44 @@ class LogBrowser():
                 for line in log:
                     print(line.message)
             else:
-                print(log[-1].message)
+                msg = log[-1].message
+                parts = msg.split('Exception ')
+                if len(parts) == 2:
+                    print(parts[1])
+                else:
+                    print(msg)
+
+    def doFailZoology(self, giveExampleId=False):
+        zoo = {}
+        examples = {}
+        fails = self.getFailRecords()
+        for fail in fails:
+            dataId = self.recordToDataId(fail)
+            log = self.logs[fail]
+            msg = log[-1].message
+            parts = msg.split('Exception ')
+            if len(parts) != 2:  # pretty sure all fails contain one and only one 'Exception' but be safe
+                self.log.warning(f'Surprise parsing log for {dataId}')
+                continue
+            else:
+                error = parts[1]
+                for error_string in self.SPECIAL_ZOO_CASES:
+                    if error.find(error_string) != -1:
+                        error = error.split(error_string)[0] + error_string
+                if error not in zoo:
+                    zoo[error] = 1
+                    if giveExampleId:
+                        examples[error] = dataId
+                else:
+                    zoo[error] += 1
+
+        pad = 0  # don't pad when giving examples, it looks weird
+        if not giveExampleId:
+            maxCount = max([v for v in zoo.values()])
+            pad = math.ceil(math.log10(maxCount))  # number of digits in the largest count
+
+        for error in sorted(zoo.keys()):
+            count = zoo[error]
+            print(f"{count:{pad}} instance{'s' if count > 1 else ' '} of {error}")
+            if giveExampleId:
+                print(f"example dataId: {examples[error]}\n")
