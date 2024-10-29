890e0387fa2b09b99af25a4c2cdbe6f03a67f178
Josh Meyers <jmeyers314@gmail.com>
2024-10-29 16:59:15 +0000

    Tweak limits

--- python/lsst/summit/extras/plotting/focusSweep.py
+++ python/lsst/summit/extras/plotting/focusSweep.py
@@ -256,29 +256,30 @@ def plotSweepParabola(data, varName, fitDict, saveAs=None, figAxes=None):
         ax.set_xlabel(label + "[" + unit + "]")
 
     for ax in [ellipSeqAx, ellipVarAx]:
-        ax.set_ylim(-0.11, 0.11)
+        ax.set_ylim(-0.21, 0.21)
         ax.axhline(0, c="k")
         ax.set_ylabel("e1 or e2")
 
     # Print useful info in the top right
     kwargs = dict(fontsize=10, ha="left", fontfamily="monospace")
-    fig.text(0.7, 0.94, "FWHM fit", **kwargs)
-    fig.text(0.7, 0.92, "--------", **kwargs)
+    xtext = 0.6
+    fig.text(xtext, 0.94, "FWHM fit", **kwargs)
+    fig.text(xtext, 0.92, "--------", **kwargs)
     fig.text(
-        0.7, 0.90, f"vertex: {fitDict['vertex']:.3f} ± {fitDict['vertexUncertainty']:.3f} {unit}", **kwargs
+        xtext, 0.90, f"vertex: {fitDict['vertex']:.3f} ± {fitDict['vertexUncertainty']:.3f} {unit}", **kwargs
     )
     fig.text(
-        0.7,
+        xtext,
         0.88,
         f"extremum: {fitDict['extremum']:.3f} ± {fitDict['extremumUncertainty']:.3f} arcsec",
         **kwargs,
     )
-    fig.text(0.7, 0.86, f"RMS resid: {fitDict['rms']:.3f} arcsec", **kwargs)
+    fig.text(xtext, 0.86, f"RMS resid: {fitDict['rms']:.3f} arcsec", **kwargs)
 
-    fig.text(0.7, 0.80, "Ellipticity spread", **kwargs)
-    fig.text(0.7, 0.78, "------------------", **kwargs)
-    fig.text(0.7, 0.76, f"e1 RMS: {fitDict['e1Rms']:.3f}", **kwargs)
-    fig.text(0.7, 0.74, f"e2 RMS: {fitDict['e2Rms']:.3f}", **kwargs)
+    fig.text(xtext, 0.80, "Ellipticity spread", **kwargs)
+    fig.text(xtext, 0.78, "------------------", **kwargs)
+    fig.text(xtext, 0.76, f"e1 RMS: {fitDict['e1Rms']:.3f}", **kwargs)
+    fig.text(xtext, 0.74, f"e2 RMS: {fitDict['e2Rms']:.3f}", **kwargs)
 
     fig.tight_layout()
     if saveAs is not None:
