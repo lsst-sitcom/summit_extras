81613c409b833f5cb1f51c43105985b30d500959
Josh Meyers <jmeyers314@gmail.com>
2025-04-21 22:44:56 +0000

    Add hists to psfAzEl plot

--- python/lsst/summit/extras/plotting/psfPlotting.py
+++ python/lsst/summit/extras/plotting/psfPlotting.py
@@ -37,10 +37,12 @@ from typing import TYPE_CHECKING
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.table import Table, vstack
+from astropy.table import vstack
+from matplotlib.gridspec import GridSpec
 from matplotlib.patches import Polygon
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-from lsst.afw.cameraGeom import FOCAL_PLANE
+from lsst.afw.cameraGeom import FIELD_ANGLE, FOCAL_PLANE
 from lsst.afw.geom.ellipses import Quadrupole
 from lsst.geom import LinearTransform, radians
 
@@ -231,7 +233,16 @@ def makeFigureAndAxes() -> tuple[plt.Figure, Any]:
     axes : `numpy.ndarray`
         The created axes.
     """
-    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(7, 6), sharex=True, sharey=True)
+    fig = plt.figure(figsize=(10, 6))
+    gs = GridSpec(2, 3, figure=fig)
+
+    axes = np.empty((2, 3), dtype=object)
+    axes[0, 0] = fig.add_subplot(gs[0, 0])
+    axes[0, 1] = fig.add_subplot(gs[0, 1], sharex=axes[0, 0], sharey=axes[0, 0])
+    axes[1, 0] = fig.add_subplot(gs[1, 0], sharex=axes[0, 0], sharey=axes[0, 0])
+    axes[1, 1] = fig.add_subplot(gs[1, 1], sharex=axes[0, 0], sharey=axes[0, 0])
+    axes[0, 2] = fig.add_subplot(gs[0, 2])
+    axes[1, 2] = fig.add_subplot(gs[1, 2])
     return fig, axes
 
 
@@ -481,35 +492,52 @@ def makeAzElPlot(
     saveAs : `str`, optional
         The file path to save the figure.
     """
+    mm_to_deg = 100 * 0.2 / 3600
     if len(table) == 0:
         return
     oneRaftOnly = camera.getName() in ["LSSTComCam", "LSSTComCamSim", "TS8"]
     # I think this is roughly right for plotting - diameter is 5x but we need
     # less border, and 4.5 looks about right by eye.
     fullCameraFactor = 4.5
-    plotLimit = 90 if oneRaftOnly else 90 * fullCameraFactor
+    plotLimit = 90 * mm_to_deg if oneRaftOnly else 90 * fullCameraFactor * mm_to_deg
     quiverScale = 5
 
     table = randomRowsPerDetector(table, maxPointsPerDetector)
 
-    cbar = addColorbarToAxes(axes[0, 1].scatter(table["aa_x"], table["aa_y"], c=table["FWHM"], s=5))
+    cbar = addColorbarToAxes(
+        axes[0, 1].scatter(table["aa_x"] * mm_to_deg, table["aa_y"] * mm_to_deg, c=table["FWHM"], s=5)
+    )
     cbar.set_label("FWHM [arcsec]")
 
     emax = np.quantile(np.abs(np.concatenate([table["e1"], table["e2"]])), 0.98)
-    axes[1, 0].scatter(table["aa_x"], table["aa_y"], c=table["aa_e1"], vmin=-emax, vmax=emax, cmap="bwr", s=5)
+    axes[1, 0].scatter(
+        table["aa_x"] * mm_to_deg,
+        table["aa_y"] * mm_to_deg,
+        c=table["aa_e1"],
+        vmin=-emax,
+        vmax=emax,
+        cmap="bwr",
+        s=5,
+    )
     axes[1, 0].text(0.05, 0.92, "e1", transform=axes[1, 0].transAxes, fontsize=10)
 
     cbar = addColorbarToAxes(
         axes[1, 1].scatter(
-            table["aa_x"], table["aa_y"], c=table["aa_e2"], vmin=-emax, vmax=emax, cmap="bwr", s=5
+            table["aa_x"] * mm_to_deg,
+            table["aa_y"] * mm_to_deg,
+            c=table["aa_e2"],
+            vmin=-emax,
+            vmax=emax,
+            cmap="bwr",
+            s=5,
         )
     )
     cbar.set_label("e")
     axes[1, 1].text(0.05, 0.92, "e2", transform=axes[1, 1].transAxes, fontsize=10)
 
     Q = axes[0, 0].quiver(
-        table["aa_x"],
-        table["aa_y"],
+        table["aa_x"] * mm_to_deg,
+        table["aa_y"] * mm_to_deg,
         table["e"] * np.cos(0.5 * np.arctan2(table["aa_e2"], table["aa_e1"])),
         table["e"] * np.sin(0.5 * np.arctan2(table["aa_e2"], table["aa_e1"])),
         headlength=0,
@@ -519,14 +547,14 @@ def makeAzElPlot(
     )
     axes[0, 1].quiverkey(Q, X=0.08, Y=0.95, U=0.2, label="0.2", labelpos="S")
 
-    for ax in axes.ravel():
+    for ax in axes[:2, :2].ravel():
         ax.set_aspect("equal")
         ax.set_xlim(-plotLimit, plotLimit)
         ax.set_ylim(-plotLimit, plotLimit)
-    for ax in axes[1, :]:
-        ax.set_xlabel("Az")
-    for ax in axes[:, 0]:
-        ax.set_ylabel("El")
+    for ax in axes[1, :2]:
+        ax.set_xlabel("$\\Delta$ Azimuth [deg]")
+    for ax in axes[:2, 0]:
+        ax.set_ylabel("$\\Delta$ Elevation [deg]")
 
     # Plot camera detector outlines for single-raft cameras
     if oneRaftOnly:
@@ -549,7 +577,7 @@ def makeAzElPlot(
             rx = aaRot[0, 0] * x + aaRot[0, 1] * y
             ry = aaRot[1, 0] * x + aaRot[1, 1] * y
             rtp = table.meta["rotTelPos"]
-            for ax in axes.ravel():
+            for ax in axes[:2, :2].ravel():
                 ax.plot(rxs, rys, c="k", lw=1, alpha=0.3)
                 ax.text(
                     rx,
@@ -572,22 +600,57 @@ def makeAzElPlot(
                     continue
                 raft = f"R{i}{j}"
                 detector_type = camera[raft + "_S00"].getPhysicalType()
-                c0 = camera[raft + "_S00"].getCorners(FOCAL_PLANE)[0]
-                c1 = camera[raft + "_S02"].getCorners(FOCAL_PLANE)[1]
-                c2 = camera[raft + "_S22"].getCorners(FOCAL_PLANE)[2]
-                c3 = camera[raft + "_S20"].getCorners(FOCAL_PLANE)[3]
-                xs = [c0.x, c1.x, c2.x, c3.x, c0.x]
-                ys = [c0.y, c1.y, c2.y, c3.y, c0.y]
+                c0 = camera[raft + "_S00"].getCorners(FIELD_ANGLE)[0]
+                c1 = camera[raft + "_S02"].getCorners(FIELD_ANGLE)[1]
+                c2 = camera[raft + "_S22"].getCorners(FIELD_ANGLE)[2]
+                c3 = camera[raft + "_S20"].getCorners(FIELD_ANGLE)[3]
+                xs = np.rad2deg([c0.x, c1.x, c2.x, c3.x, c0.x])
+                ys = np.rad2deg([c0.y, c1.y, c2.y, c3.y, c0.y])
                 xs = np.array(xs)
                 ys = np.array(ys)
                 rxs = aaRot[0, 0] * xs + aaRot[0, 1] * ys
                 rys = aaRot[1, 0] * xs + aaRot[1, 1] * ys
-                for ax in axes.ravel():
+                for ax in axes[:2, :2].ravel():
                     c = "#999999" if detector_type == "E2V" else "#DDDDDD"
                     polygon = Polygon(
                         list(zip(rxs, rys)), closed=True, fill=True, edgecolor="none", facecolor=c, alpha=0.2
                     )
                     ax.add_patch(polygon)
+
+    # Add histograms
+    fwhm_percentile = np.nanpercentile(table["FWHM"].data, [25, 50, 75])
+    e_percentile = np.nanpercentile(table["e"].data, [25, 50, 75])
+    axes[0, 2].hist(table["FWHM"], bins=int(np.sqrt(len(table))), color="C0")
+    axes[1, 2].hist(table["e"], bins=int(np.sqrt(len(table))), color="C1")
+    text_kwargs = {
+        "transform": axes[0, 2].transAxes,
+        "fontsize": 10,
+        "horizontalalignment": "right",
+        "verticalalignment": "top",
+    }
+    axes[0, 2].text(0.95, 0.95, "FWHM", **text_kwargs)
+    axes[0, 2].text(0.95, 0.89, "[arcsec]", **text_kwargs)
+    axes[0, 2].text(0.95, 0.83, f"median: {fwhm_percentile[1]:.2f}", **text_kwargs)
+    axes[0, 2].text(0.95, 0.77, f"IQR: {fwhm_percentile[2] - fwhm_percentile[0]:.2f}", **text_kwargs)
+    axes[0, 2].axvline(fwhm_percentile[1], color="k", lw=2)
+    axes[0, 2].axvline(fwhm_percentile[0], color="grey", lw=1)
+    axes[0, 2].axvline(fwhm_percentile[2], color="grey", lw=1)
+
+    axes[1, 2].text(
+        0.95,
+        0.95,
+        "e",
+        transform=axes[1, 2].transAxes,
+        fontsize=10,
+        horizontalalignment="right",
+        verticalalignment="top",
+    )
+    axes[1, 2].axvline(e_percentile[1], color="k", lw=2)
+    axes[1, 2].axvline(e_percentile[0], color="grey", lw=1)
+    axes[1, 2].axvline(e_percentile[2], color="grey", lw=1)
+    text_kwargs["transform"] = axes[1, 2].transAxes
+    axes[1, 2].text(0.95, 0.89, f"median: {e_percentile[1]:.2f}", **text_kwargs)
+    axes[1, 2].text(0.95, 0.83, f"IQR: {e_percentile[2] - e_percentile[0]:.2f}", **text_kwargs)
 
     fig.tight_layout(pad=0, h_pad=0, w_pad=0)
     if saveAs:
